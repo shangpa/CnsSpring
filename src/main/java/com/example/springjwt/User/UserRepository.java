312aaa4a41a5ca40cc728e1dd90ea8a0fb54b4be
jihyunArchive <ojhyeon930@naver.com>
2025-03-24 01:35:42 +0900

    3/24 냉장고 조회, 검색, 수정, 삭제

--- src/main/java/com/example/springjwt/User/UserRepository.java
+++ src/main/java/com/example/springjwt/User/UserRepository.java
@@ -1,12 +1,13 @@
 package com.example.springjwt.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
     Boolean existsByUsername(String username);
 
-    //username을 받아 DB 테이블에서 회원을 조회하는 메소드 작성
     UserEntity findByUsername(String username);
 
+    Optional<UserEntity> findOptionalByUsername(String username);
 }
--- src/main/java/com/example/springjwt/fridge/FridgeRepository.java
+++ src/main/java/com/example/springjwt/fridge/FridgeRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface FridgeRepository extends JpaRepository<Fridge, Long> {
-    // UserEntity의 id 필드를 기준으로 Fridge 엔티티 조회
-    List<Fridge> findByUser_Id(Long userId);
+    //조회기준
+    List<Fridge> findByUserIdOrderByUpdatedAtDesc(Long userId);
+
 }

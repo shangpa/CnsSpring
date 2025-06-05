51bb8097e4416819ebddc794dac246758270b47a
jihyunArchive <ojhyeon930@naver.com>
2025-06-06 08:39:13 +0900

    냉장고 재료 내역 수정

--- src/main/java/com/example/springjwt/fridge/FridgeRepository.java
+++ src/main/java/com/example/springjwt/fridge/FridgeRepository.java
@@ -9,6 +9,13 @@ public interface FridgeRepository extends JpaRepository<Fridge, Long> {
     //조회기준
     List<Fridge> findByUserIdOrderByUpdatedAtDesc(Long userId);
     long countByUser(UserEntity user);
-    Optional<Fridge> findByUserAndIngredientName(UserEntity user, String ingredientName);
+
+    //삭제
+    List<Fridge> findAllByUserAndIngredientName(UserEntity user, String ingredientName);
+    
+    // 냉장고 재료 차감 또는 예상 재료 조회용 (createdAt 오래된 순 정렬)
+    List<Fridge> findAllByUserAndIngredientNameOrderByCreatedAtAsc(UserEntity user, String ingredientName);
+
+    // 최신 보유 재료 정보 (예: 가장 최근에 구매한 재료 정보가 필요할 경우)
     Optional<Fridge> findTopByUserAndIngredientNameOrderByUpdatedAtDesc(UserEntity user, String ingredientName);
 }
--- src/main/java/com/example/springjwt/fridge/history/FridgeHistoryResponse.java
+++ src/main/java/com/example/springjwt/fridge/history/FridgeHistoryResponse.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
@@ -15,4 +16,6 @@ public class FridgeHistoryResponse {
     private String unit; // 예: 개, g, ml 등
     private String actionType; // ADD / USE
     private LocalDateTime actionDate;
+    private LocalDate fridgeDate;
+    private String dateOption;
 }

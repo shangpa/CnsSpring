dfc30d4943da0d3207a8bde2bfe2de8be2f815b1
(Jaemin) <(kjm9049123@gmail.com)>
2025-09-01 20:15:28 +0900

    프로필이동가능

--- src/main/java/com/example/springjwt/shorts/ShortsListDto.java
+++ src/main/java/com/example/springjwt/shorts/ShortsListDto.java
@@ -6,9 +6,11 @@ public record ShortsListDto(
         String videoUrl,
         int viewCount,
         int likeCount,
+        Long userId,
         String userName
 ) {
     public static ShortsListDto from(ShortsVideo v) {
+        Long uid = Long.valueOf((v.getUser() != null) ? v.getUser().getId() : null);
         String name = (v.getUser() != null) ? v.getUser().getUsername() : "user";
         return new ShortsListDto(
                 v.getId(),
@@ -16,6 +18,7 @@ public record ShortsListDto(
                 v.getVideoUrl(),
                 v.getViewCount(),
                 v.getLikeCount(),
+                uid,
                 name
         );
     }
